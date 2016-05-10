a5c114a608d62448844c2209b6e2e3c4fb2082f9
Xavier Foucrier <xavier.foucrier@gmail.com>
2016-05-10 16:41:41 +0200

    Main window configured
    The main window allowed the user to configure the application
    preferences.

--- Gmail notifier/Main.Designer.cs
+++ Gmail notifier/Main.Designer.cs
@@ -23,9 +23,25 @@
 		/// le contenu de cette méthode avec l'éditeur de code.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+			this.SuspendLayout();
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(564, 316);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.HelpButton = true;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Main";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Préférences";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
