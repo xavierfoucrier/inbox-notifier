ebf1f5bc899b3aba0f3171b428c5de0b41e7b1a4
Xavier Foucrier <xavier.foucrier@gmail.com>
2016-06-06 16:52:49 +0200

    Do not disturb icon
    New icon to represent a "pause" in the synchronization process.

--- Gmail notifier/Properties/Resources.Designer.cs
+++ Gmail notifier/Properties/Resources.Designer.cs
@@ -213,6 +213,16 @@ namespace notifier.Properties {
         /// <summary>
         ///   Recherche une ressource localisée de type System.Drawing.Icon semblable à (Icône).
         /// </summary>
+        internal static System.Drawing.Icon timeout {
+            get {
+                object obj = ResourceManager.GetObject("timeout", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Icon semblable à (Icône).
+        /// </summary>
         internal static System.Drawing.Icon warning {
             get {
                 object obj = ResourceManager.GetObject("warning", resourceCulture);
