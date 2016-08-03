1d3130171f5a82bf51d4ce433eeef8c5f86e0836
Xavier Foucrier <xavier.foucrier@gmail.com>
2016-08-03 18:06:29 +0200

    Language setting

--- Gmail notifier/Properties/Settings.Designer.cs
+++ Gmail notifier/Properties/Settings.Designer.cs
@@ -130,5 +130,17 @@ namespace notifier.Properties {
                 this["PrivacyNotification"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("fr")]
+        public string Language {
+            get {
+                return ((string)(this["Language"]));
+            }
+            set {
+                this["Language"] = value;
+            }
+        }
     }
 }
