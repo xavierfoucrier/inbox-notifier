dc039918d3434a7663c5c114f030c42fe12c4a59
Xavier Foucrier <xavier.foucrier@gmail.com>
2016-12-02 14:56:25 +0100

    Sets default culture to "fr-FR"
    The application default culture is now set to "fr-FR" instead of "fr" to
    fits the UI culture translations.

--- Gmail notifier/Properties/AssemblyInfo.cs
+++ Gmail notifier/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Resources;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: NeutralResourcesLanguageAttribute("fr")]
+[assembly: NeutralResourcesLanguageAttribute("fr-FR")]
