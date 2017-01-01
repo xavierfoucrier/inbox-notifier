e908c37339a5fa9fec813cc5a0869f79d5f10a9e
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-01-01 11:17:44 +0100

    Deleting unnecessary using directives

--- Gmail notifier/Program.cs
+++ Gmail notifier/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using notifier.Languages;
 using notifier.Properties;
