c71b39ce6341655a90643c1f8ff52e1b78ec2453
Xavier Foucrier <xavier.foucrier@gmail.com>
2017-01-30 23:08:55 +0100

    Update period setting
    This new setting will be used to determine the period configuration the
    user choose to set for the automatic update service, an non-exhaustive
    list could be: at startup, once a day, once a week, never.

--- Gmail notifier/Properties/Settings.Designer.cs
+++ Gmail notifier/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace notifier.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -188,6 +188,18 @@ namespace notifier.Properties {
             }
             set {
                 this["UpdateService"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public string UpdatePeriod {
+            get {
+                return ((string)(this["UpdatePeriod"]));
+            }
+            set {
+                this["UpdatePeriod"] = value;
             }
         }
     }
