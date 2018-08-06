e35ee8ea9c6d8581d341a11bea4fef9d4ceba6e0
Xavier Foucrier <xavier.foucrier@gmail.com>
2018-08-06 09:37:17 +0200

    Run at Windows startup by default
    By default the application now run at Windows startup to improves the user experience. Note that this new configuration will not update your previous settings.

--- code/Properties/Settings.Designer.cs
+++ code/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace notifier.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool RunAtWindowsStartup {
             get {
                 return ((bool)(this["RunAtWindowsStartup"]));
