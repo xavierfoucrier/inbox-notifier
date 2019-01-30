728f81e6a500f387576d71842f228eca41329e96
Xavier Foucrier <xavier.foucrier@gmail.com>
2019-01-30 17:58:49 +0100

    LOG_FILE constant
    This constant stores the name of the application log file.

--- code/Properties/Settings.Designer.cs
+++ code/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace notifier.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -347,6 +347,15 @@ namespace notifier.Properties {
         public string UPDATE_SERVICE_NAME {
             get {
                 return ((string)(this["UPDATE_SERVICE_NAME"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("gmn.log")]
+        public string LOG_FILE {
+            get {
+                return ((string)(this["LOG_FILE"]));
             }
         }
     }
