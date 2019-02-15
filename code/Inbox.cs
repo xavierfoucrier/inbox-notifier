6fcda887e2ba14d44a34990c8cc29bb6e1720fa9
Xavier Foucrier <xavier.foucrier@gmail.com>
2019-02-16 00:03:39 +0100

    Gets the exception type

--- code/Inbox.cs
+++ code/Inbox.cs
@@ -245,7 +245,7 @@ namespace notifier {
 				UI.NotificationService.Tip(Translation.error, Translation.syncErrorOccured + exception.Message, Notification.Type.Warning, 1500);
 
 				// logs the error
-				Core.Log("Sync: " + exception.Message);
+				Core.Log("Sync: " + exception.GetType().ToString() + " " + exception.Message);
 			} finally {
 				UI.notifyIcon.Text = UI.notifyIcon.Text.Split('\n')[0] + "\n" + Translation.syncTime.Replace("{time}", Time.ToLongTimeString());
 			}
