3b60edb4dd89cba4acc2ad2f002d32c7356d7dbd
Xavier Foucrier <xavier.foucrier@gmail.com>
2019-02-17 19:50:56 +0100

    Removes useless namespace

--- code/Update.cs
+++ code/Update.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Windows.Forms;
