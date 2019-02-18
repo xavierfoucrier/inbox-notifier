cf7feb34879b05ce55ae7bd1319e648b39ff9046
Xavier Foucrier <xavier.foucrier@gmail.com>
2019-02-18 14:40:43 +0100

    Increases the authentication timeout
    Since there is some informations to read before accepting the application to access your Google account, the authentication timeout need to be increased.

--- code/Properties/Settings.Designer.cs
+++ code/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace notifier.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -313,7 +313,7 @@ namespace notifier.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("20")]
+        [global::System.Configuration.DefaultSettingValueAttribute("40")]
         public uint AUTH_TIMEOUT {
             get {
                 return ((uint)(this["AUTH_TIMEOUT"]));
