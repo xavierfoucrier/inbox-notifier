a4058720a1246f84bfe945ca36bc4da10ec6ad78
Xavier Foucrier <xavier.foucrier@gmail.com>
2019-03-12 00:14:25 +0100

    Scheduler setting
    This setting allow the user to enable schedule synchronization.

--- code/Properties/Settings.Designer.cs
+++ code/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace notifier.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -347,6 +347,18 @@ namespace notifier.Properties {
         public string UPDATE_SERVICE_NAME {
             get {
                 return ((string)(this["UPDATE_SERVICE_NAME"]));
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool Scheduler {
+            get {
+                return ((bool)(this["Scheduler"]));
+            }
+            set {
+                this["Scheduler"] = value;
             }
         }
     }
