ec058bf636aea13eee0453f31e5fad53e10f21f2
Xavier Foucrier <xavier.foucrier@gmail.com>
2019-03-22 16:24:17 +0100

    Defines class attributes

--- code/Timeslot.cs
+++ code/Timeslot.cs
@@ -6,5 +6,38 @@ using System.Threading.Tasks;
 
 namespace notifier {
 	class Timeslot {
+
+		#region #attributes
+
+		/// <summary>
+		/// Day of the timeslot
+		/// </summary>
+		private DayOfWeek Day;
+
+		/// <summary>
+		/// Starting time of the timeslot
+		/// </summary>
+		private TimeSpan Start;
+
+		/// <summary>
+		/// Ending time of the timeslot
+		/// </summary>
+		private TimeSpan End;
+
+		#endregion
+
+		#region #methods
+
+		/// <summary>
+		/// Class constructor
+		/// </summary>
+		public Timeslot() {
+		}
+
+		#endregion
+
+		#region #accessors
+
+		#endregion
 	}
 }
