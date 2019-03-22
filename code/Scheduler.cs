dfc86a2bf24d1a5ed2098bfcb86a54e2ada1692d
Xavier Foucrier <xavier.foucrier@gmail.com>
2019-03-22 22:27:42 +0100

    Scheduler class

---
+++ code/Scheduler.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace notifier {
+	class Scheduler {
+
+		#region #attributes
+
+		#endregion
+
+		#region #methods
+
+		#endregion
+
+		#region #accessors
+
+		#endregion
+
+	}
+}
