8e5c355062c506b7723ce7ceda4bc7c9309420bd
Xavier Foucrier <xavier.foucrier@gmail.com>
2019-03-26 12:09:18 +0100

    Removes unused namespace

--- code/Main.cs
+++ code/Main.cs
@@ -2,7 +2,6 @@ using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Media;
 using System.Threading;
 using System.Windows.Forms;
