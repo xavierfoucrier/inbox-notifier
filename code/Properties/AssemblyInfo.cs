b15b3a8ee5e99751e62ab55737edf8a5327ac82d
Xavier Foucrier <xavier.foucrier@gmail.com>
2020-01-12 14:59:18 +0100

    Update the copyright year

--- code/Properties/AssemblyInfo.cs
+++ code/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyDescription("Notifications of new Gmail messages for Windows platforms")]
 [assembly: AssemblyCompany("Xavier Foucrier")]
 [assembly: AssemblyProduct("Inbox Notifier")]
-[assembly: AssemblyCopyright("Copyright © 2019 Inbox Notifier")]
+[assembly: AssemblyCopyright("Copyright © 2020 Inbox Notifier")]
 [assembly: AssemblyTrademark("Xavier Foucrier")]
 [assembly: AssemblyCulture("")]
 
