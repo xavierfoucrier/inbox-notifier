3f83935d64039dd56f4c71970ad6f3e7a1a166c4
Xavier Foucrier <xavier.foucrier@gmail.com>
2020-10-05 12:35:21 +0200

    Ringtone setting
    This setting allow the application to store the user ringtone state and file location.

--- code/Properties/Settings.Designer.cs
+++ code/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace notifier.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -392,6 +392,30 @@ namespace notifier.Properties {
             }
             set {
                 this["EmailAddress"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool Ringtone {
+            get {
+                return ((bool)(this["Ringtone"]));
+            }
+            set {
+                this["Ringtone"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string RingtoneFile {
+            get {
+                return ((string)(this["RingtoneFile"]));
+            }
+            set {
+                this["RingtoneFile"] = value;
             }
         }
     }
