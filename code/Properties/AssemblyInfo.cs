445223fcb99fc3242459c409f341ff6d30f8d472
Xavier Foucrier <xavier.foucrier@gmail.com>
2024-01-08 19:00:17 +0100

    Update the copyright year

--- code/Properties/AssemblyInfo.cs
+++ code/Properties/AssemblyInfo.cs
@@ -2,14 +2,14 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-// Les informations générales relatives à un assembly dépendent de 
+// Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
 [assembly: AssemblyTitle("Gmail notifications for Windows")]
 [assembly: AssemblyDescription("Notifications of new Gmail messages for Windows platforms")]
 [assembly: AssemblyCompany("Xavier Foucrier")]
 [assembly: AssemblyProduct("Inbox Notifier")]
-[assembly: AssemblyCopyright("Copyright © 2023 Inbox Notifier")]
+[assembly: AssemblyCopyright("Copyright © 2024 Inbox Notifier")]
 [assembly: AssemblyTrademark("Xavier Foucrier")]
 [assembly: AssemblyCulture("")]
 
@@ -19,8 +19,8 @@ using System.Runtime.InteropServices;
 	[assembly: AssemblyConfiguration("Release")]
 #endif
 
-// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
-// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de
 // COM, affectez la valeur true à l'attribut ComVisible sur ce type.
 [assembly: ComVisible(false)]
 
@@ -30,11 +30,11 @@ using System.Runtime.InteropServices;
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
 //      Version principale
-//      Version secondaire 
+//      Version secondaire
 //      Numéro de build
 //      Révision
 //
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.10.0.0")]
